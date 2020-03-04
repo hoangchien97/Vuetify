d0c6478aaa827805dad2ba733c96af52eb713abc
ChienHD <hoangchien@savvycomsoftware.com>
2020-03-04 17:07:57 +0700

    using slot basic

--- src/components/Common/Slot.vue
+++ src/components/Common/Slot.vue
@@ -1,13 +1,14 @@
 <template>
-  
+  <div>
+    <slot></slot>
+    <p>It's a veritable slot machine!</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: "ListComponent"
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/Learning.vue
+++ src/components/Learning.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
     <h1>Learning Vue</h1>
+    <Slot>
+      <h1>Slot test h1</h1>
+    </Slot>
+    <Slot>
+      <h2>Slot test h2</h2>
+      <p>line two</p>
+    </Slot>
   </div>
 </template>
 
 <script>
+import Slot from "./Common/Slot";
 export default {
-  name: "Learning"
+  name: "Learning",
+  components: {
+    Slot
+  },
+  data() {
+    return {
+      items: { a: 1, b: 2, c: 3 }
+    };
+  }
 };
 </script>
 
