971138eda05f6d89ace2e86fbe2df31692984c68
ChienHD <hoangchien@savvycomsoftware.com>
2020-03-04 17:39:39 +0700

    using named slot

--- src/components/Common/Slot.vue
+++ src/components/Common/Slot.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <slot></slot>
+    <slot name="title"></slot>
+    <slot name="description"></slot>
     <p>It's a veritable slot machine!</p>
   </div>
 </template>
--- src/components/Learning.vue
+++ src/components/Learning.vue
@@ -2,7 +2,8 @@
   <div>
     <h1>Learning Vue</h1>
     <Slot>
-      <h1>Slot test h1</h1>
+      <h1 style="font-weight: bold; color: red" slot="title">H1 Title from named "title" slot</h1>
+      <h1 slot="description">Text from named "description" slot</h1>
     </Slot>
     <Slot>
       <h2>Slot test h2</h2>
