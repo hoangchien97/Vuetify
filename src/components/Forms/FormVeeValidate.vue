c11c9dd03a2e859ac28ac04c32deb5a167c2631b
ChienHD <hoangchien@savvycomsoftware.com>
2020-03-10 17:08:14 +0700

    update vee validate

--- src/components/Forms/FormVeeValidate.vue
+++ src/components/Forms/FormVeeValidate.vue
@@ -1,26 +1,111 @@
 <template>
-  <ValidationProvider rules="even" v-slot="{ errors }">
-    <input
-      v-model="value"
-      name="number"
-      type="number"
-      placeholder="type something"
-    />
-    <span>{{ errors[0] }}</span>
-  </ValidationProvider>
+  <div>
+    <div id="app">
+      <v-app id="inspire">
+        <ValidationObserver ref="observer" v-slot="{}">
+          <form>
+            <ValidationProvider
+              v-slot="{ errors }"
+              name="Name"
+              rules="required|max:10"
+            >
+              <v-text-field
+                v-model="name"
+                :counter="10"
+                :error-messages="errors"
+                label="Name"
+                required
+              ></v-text-field>
+            </ValidationProvider>
+            <ValidationProvider
+              v-slot="{ errors }"
+              name="email"
+              rules="required|email"
+            >
+              <v-text-field
+                v-model="email"
+                :error-messages="errors"
+                label="E-mail"
+                required
+              ></v-text-field>
+            </ValidationProvider>
+            <ValidationProvider
+              v-slot="{ errors }"
+              name="select"
+              rules="required"
+            >
+              <v-select
+                v-model="select"
+                :items="items"
+                :error-messages="errors"
+                label="Select"
+                data-vv-name="select"
+                required
+              ></v-select>
+            </ValidationProvider>
+            <ValidationProvider
+              v-slot="{ errors }"
+              rules="required"
+              name="checkbox"
+            >
+              <v-checkbox
+                v-model="checkbox"
+                :error-messages="errors"
+                value="1"
+                label="Option"
+                type="checkbox"
+                required
+              ></v-checkbox>
+            </ValidationProvider>
+
+            <v-btn class="mr-4" @click="submit">submit</v-btn>
+            <v-btn @click="clear">clear</v-btn>
+          </form>
+        </ValidationObserver>
+      </v-app>
+    </div>
+  </div>
 </template>
 
 <script>
-// import { extend } from "vee-validate";
+import { extend } from "vee-validate";
 
-// extend("even", value => {
-//   return value % 2 === 0;
-// });
+extend("required", {
+  message: "{_field_} can not be empty"
+});
+
+extend("max", {
+  message: "{_field_} may not be greater than {length} characters"
+});
+
+extend("email", {
+  message: "Email must be valid"
+});
+
 export default {
   name: "VeeValidate",
   data: () => ({
-    value: ""
-  })
+    name: "",
+    email: "",
+    select: null,
+    items: ["Item 1", "Item 2", "Item 3", "Item 4"],
+    checkbox: null
+  }),
+
+  methods: {
+    submit() {
+      this.$refs.observer.validate().then(result => {
+        console.log(111, result);
+      });
+    },
+    clear() {
+      this.name = "";
+      this.email = "";
+      this.select = null;
+      this.checkbox = null;
+      this.$refs.observer.reset();
+    }
+  }
 };
 </script>
 
